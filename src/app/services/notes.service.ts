8e22b04462f7e12bff13ef0488bbc71c2dc805e4
rajatkhanna023@outlook.com <rajatkhanna023@outlook.com>
2025-04-22 14:16:13 +0000

    Update notes.service.ts

--- src/app/services/notes.service.ts
+++ src/app/services/notes.service.ts
@@ -2,17 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { environment } from '../environment/environment';
-
+import { map } from 'rxjs/operators';
+ 
 @Injectable({
   providedIn: 'root'
 })
 export class NotesService {
   //private apiUrl = 'https://localhost:7082/api/Notes';
   private apiUrl = `${environment.apiUrl}/Notes`; // Use the environment variable
-
+ 
   constructor(private http: HttpClient) {}
-
+ 
   getNotes(): Observable<any[]> {
-    return this.http.get<any[]>(this.apiUrl);
+    return this.http.get<any>(this.apiUrl).pipe(
+      map(response => response.data) // extract the data property
+    );
   }
+ 
 }
